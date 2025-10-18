c905ec66be01358ddf20f254a5a186cedda0a302
Saulo Miguel <saulovailma@gmail.com>
2025-10-18 18:41:44 -0300

    Estrutura de repetição com variável de controle

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,16 @@
+/*
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c ++ 
+}
+*/
+
+// MESMA FUNCIONALIDADE AMBOS, MAS A DE BAIXO A FACILIDADE É MAIOR NA HORA DE MODIFICAR POR EXEMPLO
+
+console.log('Vai começar...')
+
+for(var c  = 1; c <= 10; c ++) { 
+    console.log(`Passo ${c}` )
+}
+
+console.log('Fim!')
